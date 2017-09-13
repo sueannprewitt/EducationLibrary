06575e0cea959bbfb715bf8db8b72f02e6ea6d3d
Sue Ann <sueannprewitt@yahoo.com>
2017-09-13 15:16:28 -0400

    Adding Insert

--- EducationLibrary/StudentCollection.cs
+++ EducationLibrary/StudentCollection.cs
@@ -67,12 +67,86 @@ namespace EducationLibrary
         }
     public static Student Select(int id)
     {
-            return null;
+            var connStr = @"Server=Student05;Database=DotNetDatabase;Trusted_Connection=yes;";
+            SqlConnection connection = new SqlConnection(connStr);
+            connection.Open();
+            if (connection.State != System.Data.ConnectionState.Open)
+            {
+                Console.WriteLine("Connection did not open");
+                return null;
+            }
+            StudentCollection students = new StudentCollection();
+            var sql = $"Select * from Student where Id = {id}";
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                var id2 = reader.GetInt32(reader.GetOrdinal("Id"));
+                var firstName = reader.GetString(reader.GetOrdinal("FirstName"));
+                var lastName = reader.GetString(reader.GetOrdinal("LastName"));
+                var address = reader.GetString(reader.GetOrdinal("Address"));
+                var city = reader.GetString(reader.GetOrdinal("City"));
+                var state = reader.GetString(reader.GetOrdinal("State"));
+                var zipcode = reader.GetString(reader.GetOrdinal("Zipcode"));
+                var phoneNumber = reader.GetString(reader.GetOrdinal("PhoneNumber"));
+                var email = reader.GetString(reader.GetOrdinal("Email"));
+                var birthday = reader.GetDateTime(reader.GetOrdinal("Birthday"));
+                var majorid = -1; //means that the MajorId in the db is NULL
+                if (!reader.GetValue(reader.GetOrdinal("Majorid")).Equals(DBNull.Value))
+                {
+                    majorid = reader.GetInt32(reader.GetOrdinal("MajorId"));
+                }
+                var sat = reader.GetInt32(reader.GetOrdinal("SAT"));
+                var gpa = reader.GetDouble(reader.GetOrdinal("GPA"));
+
+                Student student = new Student();
+                student.Id = id2;
+                student.FirstName = firstName;
+                student.LastName = lastName;
+                student.Address = address;
+                student.City = city;
+                student.State = state;
+                student.Zipcode = zipcode;
+                student.PhoneNumber = phoneNumber;
+                student.Email = email;
+                student.Birthday = birthday;
+                student.MajorId = majorid;
+                student.SAT = sat;
+                student.GPA = gpa;
+                students.Add(student);
+
+            }
+            reader.Close();
+            connection.Close();
+            if(students.Count == 0)
+            {
+                return null;
+            }else
+            {
+                return students[0];
+            }
 
     }
         public static bool Insert(Student student)
         {
-            return false;
+            var connStr = @"Server=Student05;Database=DotNetDatabase;Trusted_Connection=yes;";
+            SqlConnection connection = new SqlConnection(connStr);
+            connection.Open();
+            if (connection.State != System.Data.ConnectionState.Open)
+            {
+                Console.WriteLine("Connection did not open");
+                return false;
+            }
+            var sql = $"Insert into Student (FirstName, LastName, Address, City, State, Zipcode," +
+                $"PhoneNumber, Email, Birthday, MajorId, SAT, GPA)" +
+                $"VALUES" +
+                $"('{student.FirstName}', '{student.LastName}', '{student.Address}', " +
+                $"'{student.City}', '{student.State}', '{student.Zipcode}', " +
+                $"'{student.PhoneNumber}', '{student.Email}', '{student.Birthday}', " +
+                $"{student.MajorId}, {student.SAT}, {student.GPA})";
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            var recsAffected = cmd.ExecuteNonQuery();
+            return (recsAffected == 1);
         }
         public static bool Update(Student student)
         {
--- TestEducationLibrary/Program.cs
+++ TestEducationLibrary/Program.cs
@@ -12,8 +12,23 @@ namespace TestEducationLibrary
         void Run()
         {
             StudentCollection students = StudentCollection.Select(); //calling StudentCollection Select method
-            
 
+            Student Susie = StudentCollection.Select(5);
+            Student doesNotExist = StudentCollection.Select(0); //should return null
+            Student stud = new Student();
+            stud.FirstName = "Test";
+            stud.LastName = "Test";
+            stud.Address = "Test";
+            stud.City = "Test";
+            stud.State = "OH";
+            stud.Zipcode = "Test";
+            stud.Birthday = new DateTime(2017, 9, 13);
+            stud.PhoneNumber = "5135551212";
+            stud.Email = "a@b.c";
+            stud.MajorId = 6;
+            stud.SAT = 1600;
+            stud.GPA = 5.0;
+            bool rc = StudentCollection.Insert(stud);
 
         }
         static void Main(string[] args)
